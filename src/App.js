bc6a778d0ddf0535a33d68b812b0eefc16bf0b06
Aman Gupta <aman.gupta@squareboat.com>
2023-04-11 11:28:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import axios from 'axios';
+import { API_BASE_URL } from './constants';
 
 function App() {
+  function handleClick(device){
+    axios.get(API_BASE_URL+"/house/"+device+"/toggle")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className='container'>
+          <button className='switch-buttons' onClick={()=>{handleClick("light")}}>
+            <div className="button-contents">
+              <i class="fa-regular fa-lightbulb"></i>
+              <span>
+                Bulb
+              </span>
+            </div>
+            
+        </button>
+        <button className='switch-buttons' onClick={()=>{handleClick("fan")}}>
+            
+            <div className="button-contents">
+              <i class='fas fa-fan'></i>
+              <span>
+                Fan
+              </span>
+            </div>
+        </button>
+      <button className='switch-buttons' onClick={()=>{handleClick("socket")}}>
+        <div className="button-contents">
+                      <i class='fas fa-plug'></i>
+                    <span>
+                      Socket
+                    </span>
+        </div>
+      </button>
+      <button className='switch-buttons' onClick={()=>{handleClick("ac")}}>
+        <div className="button-contents">
+                      <i class='fas fa-plug'></i>
+                    <span>
+                      AC
+                    </span>
+        </div>
+            
+      </button>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+export default function Header(props){
+    return(
+        <div className="header">
+            Device Automation 
+        </div>
+    )
+}
